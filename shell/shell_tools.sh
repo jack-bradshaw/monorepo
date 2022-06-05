1db4e8fb874d5efc384dd0af06f023d5c6c0cbb9
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-05 12:13:36 -0700

    Renamed shell tool comments.

--- shell/shell_tools.sh
+++ shell/shell_tools.sh
@@ -2,14 +2,14 @@
 
 # Tools for working with bash.
 
-reload_shell() {
+reinit() {
   clear
   source $HOME/.bashrc
 }
 
 SETUP_SCRIPT=https://raw.githubusercontent.com/matthewbradshaw-io/monorepo/main/shell/setup_local.sh
 
-revolve_shell() {
+rebuild() {
   bash <(curl -s $SETUP_SCRIPT)
   reload_shell
 }
