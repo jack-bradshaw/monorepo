306211190b2d821710b141b48bc6fcfd0b7932d1
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-08 20:25:02 -0700

    Added function to install bazel.

--- shell/bazel_tools.sh
+++ shell/bazel_tools.sh
@@ -15,3 +15,16 @@ bbr() {
 buildifier() {
   bazel run //:buildifier
 }
+
+install_bazel() {
+  sudo apt install apt-transport-https curl gnupg
+  curl -fsSL https://bazel.build/bazel-release.pub.gpg \
+      | gpg --dearmor > bazel.gpg
+  sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+  echo "deb [arch=amd64] \
+      https://storage.googleapis.com/bazel-apt stable jdk1.8" \
+      | sudo tee /etc/apt/sources.list.d/bazel.list
+
+  sudo apt update && sudo apt install bazel
+  sudo apt update && sudo apt full-upgrade
+}
