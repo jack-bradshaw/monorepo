ff26a45cdf433e56c5fd05c2e193125d59fc1d07
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-09 22:10:44 -0700

    Fixed remaining merge conflicts.

--- java/io/matthewbradshaw/omniverse/core/Main.java
+++ java/io/matthewbradshaw/omniverse/core/Main.java
@@ -10,27 +10,6 @@ import com.jme3.app.LostFocusBehavior;
 
 public class Main {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    AppSettings settings = new AppSettings(/* loadDefaults= */ true);
-    settings.put(VRConstants.SETTING_VRAPI, VRConstants.SETTING_VRAPI_OPENVR_LWJGL_VALUE);
-    settings.put(VRConstants.SETTING_ENABLE_MIRROR_WINDOW, true);
-
-    VREnvironment env = new VREnvironment(settings);
-    env.initialize();
-
-    if (env.isInitialized()){
-      VRAppState vrAppState = new VRAppState(settings, env);
-      vrAppState.setMirrorWindowSize(1024, 800);
-      App app = new App(vrAppState);
-      app.setLostFocusBehavior(LostFocusBehavior.Disabled);
-      app.setSettings(settings);
-      app.setShowSettings(false);
-      app.start();
-    } else {
-      throw new IllegalStateException("Root environment initialization failed.");
-    }
-=======
     Game.main();
->>>>>>> 33a75b95c4d7c15ceb38abab6f9d885cd30827f5
   }
 }
