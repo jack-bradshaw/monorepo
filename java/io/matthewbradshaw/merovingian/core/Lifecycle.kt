7dedd19f0f3dab0533ec10816211d505e7fb067f
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-17 12:38:07 -0700

    Cleanup
    Original-Subject: Merge pull request #41 from matthewbradshaw-io/cleanup

--- java/io/matthewbradshaw/merovingian/core/Lifecycle.kt
+++
@@ -1,6 +0,0 @@
-package java.io.matthewbradshaw.merovingian.core
-
-interface Lifecycle {
-  suspend fun start()
-  suspend fun stop()
-}
--- java/io/matthewbradshaw/merovingian/core/Sustainable.kt
+++
@@ -1,5 +0,0 @@
-package java.io.matthewbradshaw.merovingian.core
-
-interface Sustainable {
-  val lifecycle: Lifecycle
-}
