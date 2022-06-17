a4825af6fe5d3e740cbcf9f78b6e706dbf8e1a6b
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-17 16:28:20 -0700

    Fixed unresolved merge conflicts.

--- BUILD
+++ BUILD
@@ -3,10 +3,7 @@ load("@com_github_bazelbuild_buildtools//buildifier:def.bzl", "buildifier")
 buildifier(
     name = "buildifier",
 )
-<<<<<<< HEAD
-=======
 
 load("@dagger//:workspace_defs.bzl", "dagger_rules")
 
 dagger_rules()
->>>>>>> c57fad40b5c4f0a93c121aecd463635626b91646
--- java/io/matthewbradshaw/omniverse/core/Main.java
+++ java/io/matthewbradshaw/omniverse/core/Main.java
@@ -8,11 +8,8 @@ import com.jme3.app.VRConstants;
 import com.jme3.app.VREnvironment;
 import com.jme3.app.LostFocusBehavior;
 
-<<<<<<< HEAD
-=======
 import java.io.matthewbradshaw.octavius.application.Game;
 
->>>>>>> c57fad40b5c4f0a93c121aecd463635626b91646
 public class Main {
   public static void main(String[] args) {
     Game.main();
