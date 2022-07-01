2d9bce817156fc7b39e27b920eeae46483a28635
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-30 21:48:58 -0700

    Refactored HostImpl
    Added mutex to guard item switch.

--- java/io/matthewbradshaw/merovingian/host/HostImpl.kt
+++ java/io/matthewbradshaw/merovingian/host/HostImpl.kt
@@ -4,20 +4,25 @@ import io.matthewbradshaw.merovingian.engine.Engine
 import io.matthewbradshaw.merovingian.model.WorldItem
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class HostImpl @Inject internal constructor(
   private val engine: Engine,
 ) : Host {
 
-  private var logic: Job? = null
+  private val mutex = Mutex()
+  private var activeLogic: Job? = null
 
   override suspend fun run(item: WorldItem) {
-    engine.extractCoroutineScope().launch {
-      logic?.let { it.cancel() }
-      engine.extractRootNode().detachAllChildren()
-      engine.extractRootNode().attachChild(item.representation())
-      logic = launch { item.logic() }
+    mutex.withLock {
+      engine.extractCoroutineScope().launch {
+        activeLogic?.let { it.cancel() }
+        engine.extractRootNode().detachAllChildren()
+        engine.extractRootNode().attachChild(item.representation())
+        activeLogic = launch { item.logic() }
+      }
     }
   }
 }
