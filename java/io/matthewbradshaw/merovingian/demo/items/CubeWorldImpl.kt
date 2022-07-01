967eaecfef57cb252cdabe538aff30f724b98068
Jack Bradshaw <jack@jack-bradshaw.com>
2022-06-30 23:12:04 -0700

    Update CubeWorldImpl.kt
    Added floor.

--- java/io/matthewbradshaw/merovingian/demo/items/CubeWorldImpl.kt
+++ java/io/matthewbradshaw/merovingian/demo/items/CubeWorldImpl.kt
@@ -1,41 +1,46 @@
 package io.matthewbradshaw.merovingian.demo.items
 
 import com.jme3.math.Vector3f
+import com.jme3.scene.Geometry
+import com.jme3.scene.Node
 import com.jme3.scene.Spatial
+import com.jme3.scene.shape.Box
 import io.matthewbradshaw.klu.concurrency.once
-import io.matthewbradshaw.merovingian.demo.DemoScope
 import io.matthewbradshaw.merovingian.demo.materials.Materials
 import io.matthewbradshaw.merovingian.engine.Engine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Provider
 
-@DemoScope
 class CubeWorldImpl @Inject internal constructor(
   private val cubeSwarmProvider: Provider<CubeSwarm>,
   private val engine: Engine,
   private val materials: Materials,
 ) : CubeWorld {
 
-  //private lateinit var root: Node
+  private lateinit var coordinator: Node
   private lateinit var swarm: CubeSwarm
-  //private lateinit var floor: Spatial
+  private lateinit var floor: Spatial
 
   private val preparations = once {
-    // root = Node("root")
+    swarm = cubeSwarmProvider.get().apply {
+      setCubeCount(CUBE_COUNT)
+    }
 
-    swarm = cubeSwarmProvider.get()
+    floor = Geometry("cube_box", Box(2f, 0.2f, 2f)).apply {
+      setMaterial(materials.getRandomly())
+      setLocalTranslation(0f, 0f, 0f)
+    }
 
-    /*if (!this::floor.isInitialized) {
-      floor = Geometry("cube_box", Box(5f, 0.2f, 5f)).apply {
-        setMaterial(materials.createUnshadedGreen())
-      }
-    }*/
+    coordinator = Node("coordinator").apply {
+      attachChild(swarm.representation())
+      attachChild(floor)
+    }
   }
 
   override suspend fun representation(): Spatial {
     preparations.runIfNeverRun()
-    return swarm.representation()
+    return coordinator
   }
 
   override suspend fun logic() {
