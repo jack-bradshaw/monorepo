1d088f3b90352de6152c0f3b3b5a246099942d90
Jack Bradshaw <jack@jack-bradshaw.com>
2022-07-03 14:29:48 -0700

    Moved setup code to correct diaptchers

--- java/io/matthewbradshaw/merovingian/clock/PhysicsClock.kt
+++ java/io/matthewbradshaw/merovingian/clock/PhysicsClock.kt
@@ -5,11 +5,13 @@ import com.jme3.bullet.PhysicsTickListener
 import com.jme3.bullet.control.GhostControl
 import com.jme3.scene.Node
 import io.matthewbradshaw.merovingian.MerovingianScope
+import io.matthewbradshaw.merovingian.coroutines.renderingDispatcher
 import io.matthewbradshaw.merovingian.engine.Engine
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @MerovingianScope
@@ -18,31 +20,34 @@ class PhysicsClock @Inject internal constructor(
 ) : Clock {
 
   private var totalRuntime = 0.0
+  private val totalFlow = MutableStateFlow<Double>(0.0)
+  private val deltaFlow = MutableStateFlow<Double>(0.0)
 
   init {
-    val ghostControl = object : GhostControl(), PhysicsTickListener {
-      override fun physicsTick(space: PhysicsSpace, tpf: Float) = runBlocking {
-        totalRuntime += tpf
+    runBlocking {
+      val ghostControl = object : GhostControl(), PhysicsTickListener {
+        override fun physicsTick(space: PhysicsSpace, tpf: Float) = runBlocking {
+          totalRuntime += tpf
+        }
+
+        override fun prePhysicsTick(space: PhysicsSpace, tpf: Float) {}
+      }
+      val ghost = Node("physics_clock_ghost").apply { addControl(ghostControl) }
+
+      withContext(engine.renderingDispatcher()) {
+        engine.extractFrameworkNode().attachChild(ghost)
       }
 
-      override fun prePhysicsTick(space: PhysicsSpace, tpf: Float) { /* unused */
-      }
-    }
-    val ghost = Node("physics_clock_ghost").apply { addControl(ghostControl) }
-
-    engine.extractFrameworkNode().attachChild(ghost)
-    engine.extractPhysics().getPhysicsSpace().add(ghostControl)
-    engine.extractCoroutineScope().launch(Dispatchers.Default) {
-      while (true) {
-        val previousTotalRuntime = totalFlow.value
-        totalFlow.value = totalRuntime
-        deltaFlow.value = totalRuntime - previousTotalRuntime
+      engine.extractPhysics().getPhysicsSpace().add(ghostControl)
+      engine.extractCoroutineScope().launch(Dispatchers.Default) {
+        while (true) {
+          val previousTotalRuntime = totalFlow.value
+          totalFlow.value = totalRuntime
+          deltaFlow.value = totalRuntime - previousTotalRuntime
+        }
       }
     }
   }
-
-  private val totalFlow = MutableStateFlow<Double>(0.0)
-  private val deltaFlow = MutableStateFlow<Double>(0.0)
 
   override fun totalSec() = totalFlow
   override fun deltaSec() = deltaFlow
