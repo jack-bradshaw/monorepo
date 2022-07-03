1f0b602a253960e8d7560f4cdc27d8c41f2f725b
Jack Bradshaw <jack@jack-bradshaw.com>
2022-07-03 14:17:34 -0700

    Added physics to world item and host

--- java/io/matthewbradshaw/merovingian/host/HostImpl.kt
+++ java/io/matthewbradshaw/merovingian/host/HostImpl.kt
@@ -6,9 +6,14 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import com.jme3.bullet.collision.PhysicsCollisionObject
 import javax.inject.Inject
 import com.jme3.scene.Spatial
+import io.matthewbradshaw.merovingian.coroutines.physicsPreTickDispatcher
 import io.matthewbradshaw.merovingian.coroutines.renderingDispatcher
+
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.Dispatchers
 
 class HostImpl @Inject internal constructor(
   private val engine: Engine,
@@ -16,16 +21,24 @@ class HostImpl @Inject internal constructor(
 
   private val mutex = Mutex()
   private var activeRepresentation: Spatial? = null
+  private var activePhysics: PhysicsCollisionObject? = null
   private var activeLogic: Job? = null
-
 
   override suspend fun run(item: WorldItem) {
     mutex.withLock {
-      engine.extractCoroutineScope().launch(engine.renderingDispatcher()) {
-        activeLogic?.let { it.cancel() }
-        activeRepresentation?.let { engine.extractFrameworkNode().detachChild(it) }
-        activeRepresentation = item.representation().also { engine.extractApplicationNode().attachChild(it) }
-        activeLogic = launch { item.logic() }
+      engine.extractCoroutineScope().launch {
+        withContext(Dispatchers.Default) {
+          activeLogic?.let { it.cancel() }
+          activePhysics?.let { engine.extractPhysics().getPhysicsSpace().remove(it) }
+        }
+        withContext(engine.renderingDispatcher()) {
+          activeRepresentation?.let { engine.extractFrameworkNode().detachChild(it) }
+          activeRepresentation = item.representation().also { engine.extractApplicationNode().attachChild(it) }
+        }
+        withContext(Dispatchers.Default) {
+          activePhysics = item.physics().also { engine.extractPhysics().getPhysicsSpace().add(it) }
+          activeLogic = launch { item.logic() }
+        }
       }
     }
   }
--- java/io/matthewbradshaw/merovingian/model/WorldItem.kt
+++ java/io/matthewbradshaw/merovingian/model/WorldItem.kt
@@ -1,8 +1,10 @@
 package io.matthewbradshaw.merovingian.model
 
 import com.jme3.scene.Spatial
+import com.jme3.bullet.collision.PhysicsCollisionObject
 
 interface WorldItem {
   suspend fun representation(): Spatial
   suspend fun logic() = Unit
+  suspend fun physics(): PhysicsCollisionObject? = null
 }
