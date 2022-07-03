06498c3432dfb796a9438733e5266c27beb52570
Jack Bradshaw <jack@jack-bradshaw.com>
2022-07-03 16:28:04 -0700

    Moved physics to physics dispatcher

--- java/io/matthewbradshaw/merovingian/host/HostImpl.kt
+++ java/io/matthewbradshaw/merovingian/host/HostImpl.kt
@@ -1,19 +1,18 @@
 package io.matthewbradshaw.merovingian.host
 
+import com.jme3.bullet.collision.PhysicsCollisionObject
+import com.jme3.scene.Spatial
+import io.matthewbradshaw.merovingian.coroutines.physicsDispatcher
+import io.matthewbradshaw.merovingian.coroutines.renderingDispatcher
 import io.matthewbradshaw.merovingian.engine.Engine
 import io.matthewbradshaw.merovingian.model.WorldItem
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import com.jme3.bullet.collision.PhysicsCollisionObject
-import javax.inject.Inject
-import com.jme3.scene.Spatial
-import io.matthewbradshaw.merovingian.coroutines.physicsPreTickDispatcher
-import io.matthewbradshaw.merovingian.coroutines.renderingDispatcher
-
 import kotlinx.coroutines.withContext
-import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
 class HostImpl @Inject internal constructor(
   private val engine: Engine,
@@ -29,14 +28,18 @@ class HostImpl @Inject internal constructor(
       engine.extractCoroutineScope().launch {
         withContext(Dispatchers.Default) {
           activeLogic?.let { it.cancel() }
+        }
+        withContext(engine.physicsDispatcher()) {
           activePhysics?.let { engine.extractPhysics().getPhysicsSpace().remove(it) }
         }
         withContext(engine.renderingDispatcher()) {
           activeRepresentation?.let { engine.extractFrameworkNode().detachChild(it) }
           activeRepresentation = item.representation().also { engine.extractApplicationNode().attachChild(it) }
         }
-        withContext(Dispatchers.Default) {
+        withContext(engine.physicsDispatcher()) {
           activePhysics = item.physics().also { engine.extractPhysics().getPhysicsSpace().add(it) }
+        }
+        withContext(Dispatchers.Default) {
           activeLogic = launch { item.logic() }
         }
       }
