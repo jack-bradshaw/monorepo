310e1df4016d485d9aaa857c26f28f45121d9c66
Jack Bradshaw <jack@jack-bradshaw.com>
2022-07-03 16:25:07 -0700

    Merged physics dispatchers and modified implementation

--- java/io/matthewbradshaw/merovingian/coroutines/Dispatchers.kt
+++ java/io/matthewbradshaw/merovingian/coroutines/Dispatchers.kt
@@ -12,8 +12,8 @@ import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.CoroutineContext
 
 /**
- * Dispatcher for posting to the JMonkey Engine 3
- * [rendering loop](https://wiki.jmonkeyengine.org/docs/3.4/core/app/update_loop.html).
+ * Dispatcher for posting to the JMonkey Engine 3 [rendering update loop]
+ * (https://wiki.jmonkeyengine.org/docs/3.4/core/app/update_loop.html).
  */
 class JMonkeyRenderingDispatcher(private val engine: Engine) : CoroutineDispatcher() {
   override fun dispatch(context: CoroutineContext, block: Runnable) {
@@ -22,78 +22,26 @@ class JMonkeyRenderingDispatcher(private val engine: Engine) : CoroutineDispatch
 }
 
 /**
- * Dispatcher for posting to the JMonkey Engine 3 physics
- * [pre-tick loop](https://wiki.jmonkeyengine.org/docs/3.4/physics/collision/physics_listeners.html#how-to-listen-to-physics-ticks).
+ * Dispatcher for posting to the JMonkey Engine 3 [physics update loop]
+ * (https://wiki.jmonkeyengine.org/docs/3.4/physics/collision/physics_listeners.html#how-to-listen-to-physics-ticks).
  */
-class JMonkeyPhysicsPreTickDispatcher(private val engine: Engine) : CoroutineDispatcher() {
-
-  private val pendingRunnables = SimpleDoubleListBuffer<Runnable>()
-
-  init {
-    val ghostControl = object : GhostControl(), PhysicsTickListener {
-      override fun prePhysicsTick(space: PhysicsSpace, tpf: Float) = runBlocking {
-        pendingRunnables.switch()
-        pendingRunnables.getInactive().forEach { it.run() }
-      }
-
-      override fun physicsTick(space: PhysicsSpace, tpf: Float) { /* unused */
-      }
-    }
-    val ghost = Node("physics_pre_tick_dispatcher_ghost").apply { addControl(ghostControl) }
-    engine.extractFrameworkNode().attachChild(ghost)
-    engine.extractPhysics().getPhysicsSpace().add(ghostControl)
-  }
-
+class JMonkeyPhysicsDispatcher(private val engine: Engine) : CoroutineDispatcher() {
   override fun dispatch(context: CoroutineContext, block: Runnable) {
     runBlocking {
-      pendingRunnables.getActive().add(block)
+      engine.extractPhysics().getPhysicsSpace.enqueue(block)
     }
   }
 }
 
 /**
- * Dispatcher for posting to the JMonkey Engine 3 physics
- * [on-tick loop](https://wiki.jmonkeyengine.org/docs/3.4/physics/collision/physics_listeners.html#how-to-listen-to-physics-ticks).
- */
-class JMonkeyPhysicsOnTickDispatcher(private val engine: Engine) : CoroutineDispatcher() {
-  private val pendingRunnables = SimpleDoubleListBuffer<Runnable>()
-
-  init {
-    val ghostControl = object : GhostControl(), PhysicsTickListener {
-      override fun physicsTick(space: PhysicsSpace, tpf: Float) = runBlocking {
-        pendingRunnables.switch()
-        pendingRunnables.getInactive().forEach { it.run() }
-      }
-
-      override fun prePhysicsTick(space: PhysicsSpace, tpf: Float) { /* unused */
-      }
-    }
-    val ghost = Node("physics_pre_tick_dispatcher_ghost").apply { addControl(ghostControl) }
-    engine.extractFrameworkNode().attachChild(ghost)
-    engine.extractPhysics().getPhysicsSpace().add(ghostControl)
-  }
-
-  override fun dispatch(context: CoroutineContext, block: Runnable) {
-    runBlocking {
-      pendingRunnables.getActive().add(block)
-    }
-  }
-}
-
-/**
- * Map from application to dispatcher. Ensures safe concurrency and exactly one dispatcher per application.
+ * Existing rendering dispatchers by engine.
  */
 private val RENDERING_DISPATCHERS = ConcurrentHashMap<Engine, JMonkeyRenderingDispatcher>()
 
 /**
- * Map from application to dispatcher. Ensures safe concurrency and exactly one dispatcher per application.
+ * Existing physics dispatchers by engine.
  */
-private val PHYSICS_PRE_TICK_DISPATCHERS = ConcurrentHashMap<Engine, JMonkeyPhysicsPreTickDispatcher>()
-
-/**
- * Map from application to dispatcher. Ensures safe concurrency and exactly one dispatcher per application.
- */
-private val PHYSICS_ON_TICK_DISPATCHERS = ConcurrentHashMap<Engine, JMonkeyPhysicsOnTickDispatcher>()
+private val PHYSICS_DISPATCHERS = ConcurrentHashMap<Engine, JMonkeyPhysicsDispatcher>()
 
 /**
  * Gets a CoroutineDispatcher for this application. Every call for a given application returns the same instance, and
@@ -104,11 +52,5 @@ fun Engine.renderingDispatcher(): CoroutineDispatcher =
 
 /**
  */
-fun Engine.physicsPreTickDispatcher(): CoroutineDispatcher =
-  PHYSICS_PRE_TICK_DISPATCHERS.getOrPut(this) { JMonkeyPhysicsPreTickDispatcher(this) }
-
-/**
-
- */
-fun Engine.physicsOnTickDispatcher(): CoroutineDispatcher =
-  PHYSICS_ON_TICK_DISPATCHERS.getOrPut(this) { JMonkeyPhysicsOnTickDispatcher(this) }
+fun Engine.physicsDispatcher(): CoroutineDispatcher =
+  PHYSICS_DISPATCHERS.getOrPut(this) { JMonkeyPhysicsPreTickDispatcher(this) }
