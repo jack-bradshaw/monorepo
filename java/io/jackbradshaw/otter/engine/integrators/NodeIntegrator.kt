105474328710a244c1c1c9d80f5d662ba7d89168
Jack Bradshaw <jack@jack-bradshaw.com>
2022-08-20 22:16:28 -0700

    Fixed missing import.

--- java/io/jackbradshaw/otter/engine/integrators/NodeIntegrator.kt
+++ java/io/jackbradshaw/otter/engine/integrators/NodeIntegrator.kt
@@ -1,17 +1,17 @@
 package io.jackbradshaw.otter.engine.integrators
 
 import io.jackbradshaw.otter.engine.Engine
-import com.jme3.scene.Spatial
+import com.jme3.scene.Node
 
 class NodeIntegrator(
     private val engine: Engine
 ) : Integrator<Node> {
 
-  override suspend fun integrate(element: Spatial) {
+  override suspend fun integrate(element: Node) {
     engine.extractGameNode().attachChild(element)
   }
 
-  override suspend fun disintegrate(element: Spatial) {
+  override suspend fun disintegrate(element: Node) {
     engine.extractGameNode().detachChild(element)
   }
 }
