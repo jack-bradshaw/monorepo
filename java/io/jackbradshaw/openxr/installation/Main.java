2af47c63398c48464166c6893ff6dda5b57e11ca
Jack Bradshaw <jack@jack-bradshaw.com>
2022-09-05 11:35:31 -0700

    Deleted main testing

--- java/io/jackbradshaw/openxr/installation/Main.java
+++
@@ -1,9 +0,0 @@
-package java.io.jackbradshaw.openxr.installation;
-
-import io.jackbradshaw.otter.openxr.installation.MainKt;
-
-public class Main {
-  public static void main(String[] args) {
-    new MainKt().main();
-  }
-}
