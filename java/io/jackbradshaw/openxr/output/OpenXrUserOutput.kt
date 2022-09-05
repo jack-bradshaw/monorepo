80d9ea1c80826d1553ed9a1f0d33d221a0eb7492
Jack Bradshaw <jack@jack-bradshaw.com>
2022-09-05 12:16:46 -0700

    Deleted unnecessary input/output types

--- java/io/jackbradshaw/openxr/output/OpenXrUserOutput.kt
+++
@@ -1,6 +0,0 @@
-package io.jackbradshaw.otter.openxr.output
-
-interface OpenXrUserOutput {
-  suspend fun engageHapticFeedback(intensityProportion: Float)
-  suspend fun disengageHapticFeedback()
-}
--- java/io/jackbradshaw/openxr/output/OpenXrUserOutputs.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.otter.openxr.output
-
-import io.jackbradshaw.otter.openxr.model.User
-import io.jackbradshaw.otter.openxr.model.Identifier
-import io.jackbradshaw.otter.openxr.model.Location
-
-interface OpenXrUserOutputs {
-  suspend fun getOutput(user: User): OpenXrUserOutput
-}
