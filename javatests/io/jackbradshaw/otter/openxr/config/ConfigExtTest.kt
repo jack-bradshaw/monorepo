f9fe24a050e001de3206d62f6b2c68bc49e9e952
Jack Bradshaw <jack@jack-bradshaw.com>
2022-10-01 16:25:41 -0700

    Fixed broken test

--- javatests/io/jackbradshaw/otter/openxr/config/ConfigExtTest.kt
+++ javatests/io/jackbradshaw/otter/openxr/config/ConfigExtTest.kt
@@ -4,16 +4,17 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import kotlinx.coroutines.runBlocking
 
 @RunWith(JUnit4::class)
 class ConfigExtTest {
   @Test
   fun defaultConfig_usesTempDirectoryForActionManifestDirectory() {
-    assertThat(defaultConfig).isEqualTo(config {
-      actionManifestDirectory = System.getProperty("java.io.tmpdir")
-      actionManifestFilename = "otter_action_manifest.json"
-      actionSetName = "main"
-    })
+    assertThat(defaultConfig)
+        .isEqualTo(
+            Config.newBuilder()
+                .setActionManifestDirectory(System.getProperty("java.io.tmpdir"))
+                .setActionManifestFilename("otter_action_manifest.json")
+                .setActionSetName("main")
+                .build())
   }
 }
