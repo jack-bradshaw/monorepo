489c8f7cd44c7cac2c12e7bbcdc7671531fb31f1
Jack Bradshaw <jack@jack-bradshaw.com>
2023-01-14 13:44:31 -0800

    Fixed incorrect variable references.

--- shell/setup_local.sh
+++ shell/setup_local.sh
@@ -15,7 +15,7 @@ update_head() {
 # Configures the shell to use ZSH.
 setup_zsh() {
   rm -rf $HOME/.zshrc
-  cp $LOCAL/shell/.zshrc $HOME
+  cp $LOCAL_HEAD/shell/.zshrc $HOME
 
   touch $HOME/.zshlocal
 
@@ -26,13 +26,13 @@ setup_zsh() {
 # Exports the .gitconfig file to local home.
 export_gitconfig() {
   rm -rf $HOME/.gitconfig
-  cp $LOCAL/shell/.gitconfig $HOME
+  cp $LOCAL_HEAD/shell/.gitconfig $HOME
 }
 
 # Exports the .vimrc file to local home
 export_vimrc() {
   rm -rf $HOME/.vimrc
-  cp $LOCAL/shell/.vimrc $HOME
+  cp $LOCAL_HEAD/shell/.vimrc $HOME
 }
 
 # Decrypts and exports ssh keys to local home.
@@ -43,7 +43,7 @@ export_ssh_keys() {
   read -r -p "Decrypt SSH keys? (Y/N) " response
     if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
       gpg --output $HOME/.ssh/jackbradshaw --decrypt \
-          $LOCAL/shell/.ssh/jackbradshaw_private.gpg
+          $LOCAL_HEAD/shell/.ssh/jackbradshaw_private.gpg
       chmod 400 $HOME/.ssh/jackbradshaw # self-read-only
     fi
 }
@@ -56,7 +56,7 @@ export_gpg_keys() {
   read -r -p "Decrypt GPG keys? (Y/N) " response 
   if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
     gpg --output $HOME/.gpgkeys/jackbradshaw --decrypt \
-        $LOCAL/shell/.gpgkeys/jackbradshaw_private.gpg
+        $LOCAL_HEAD/shell/.gpgkeys/jackbradshaw_private.gpg
     chmod 400 $HOME/.gpgkeys/jackbradshaw # self-read-only
   fi
 }
