855774a4533449fe8ba211f3748e0e866bb4795a
Jack Bradshaw <jack@jack-bradshaw.com>
2023-02-25 14:42:43 -0800

    Changed comments

--- presubmit/all_targets_pass.sh
+++ presubmit/all_targets_pass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Presubmit to make sure all targets build.
-# Success condition: All targets build successfully.
+# Success condition: All targets build.
 # Failure condition: At least one target fails to build.
 
 bazel build //...
--- presubmit/all_tests_pass.sh
+++ presubmit/all_tests_pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Presubmit to make sure all tests are passing.
+# Presubmit to make sure all tests pass.
 # Success condition: All tests build and pass.
 # Failure condition: At least one test fails to build or does not pass.
 
