45c742d70f983fe409f246950a5eaad1614ab2fb
Jack Bradshaw <jack@jack-bradshaw.com>
2023-03-12 16:40:27 -0700

    Deleted unused utilities in KLU and upgraded other ones.
    Original-Subject: Merge pull request #109 from jack-bradshaw/klu

--- java/io/jackbradshaw/klu/flow/Flower.kt
+++
@@ -1,8 +0,0 @@
-package io.jackbradshaw.klu.flow
-
-import kotlinx.coroutines.flow.Flow
-
-interface Flower<T> {
-  suspend fun get(): T
-  fun asFlow(): Flow<T>
-}
--- java/io/jackbradshaw/klu/flow/Operators.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.klu.flow
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.fold
-
-suspend fun <K, V> Flow<Pair<K, V>>.toMap() =
-    fold(mutableMapOf<K, V>()) { accumulator, element ->
-      accumulator.also { it[element.first] = element.second }
-    }
