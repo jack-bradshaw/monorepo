d9ef38cd5ed2273d9c4e68b489ed394eb999ffbf
Jack Bradshaw <jack@jack-bradshaw.com>
2023-03-12 16:58:07 -0700

    Fixed error where some presubmits could fail and check would still pass.

--- presubmit/manifest.sh
+++ presubmit/manifest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Root manifest for other presubmits.
-# Should be run from the root directory.
+# Should be run from the root monorepo directory.
+
+set -e # Fail if one check fails.
 
 bash presubmit/all_build_files_formatted.sh
 bash presubmit/all_kotlin_files_formatted.sh
