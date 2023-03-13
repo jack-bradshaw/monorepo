5fe31301d88e0797f33bfbf8195367e61425ae3f
Jack Bradshaw <jack@jack-bradshaw.com>
2023-03-12 21:07:04 -0700

    Autoformatting

--- java/io/jackbradshaw/kmonkey/coroutines/Dispatchers.kt
+++ java/io/jackbradshaw/kmonkey/coroutines/Dispatchers.kt
@@ -6,7 +6,6 @@ import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineDispatcher
 
-
 /** A [CoroutineDispatcher] that posts to the rendering thread of [application]. */
 class JMonkeyRenderingDispatcher(private val application: Application) : CoroutineDispatcher() {
   override fun dispatch(context: CoroutineContext, block: Runnable) {
@@ -22,19 +21,22 @@ class JMonkeyPhysicsDispatcher(private val physicsSpace: PhysicsSpace) : Corouti
 }
 
 /**
- * A cache of rendering dispatchers, keyed by [Application]. To ensure the next call to [renderingDispatcher] returns
- * a new dispatcher, remove the map entry for the application from here.
+ * A cache of rendering dispatchers, keyed by [Application]. To ensure the next call to
+ * [renderingDispatcher] returns a new dispatcher, remove the map entry for the application from
+ * here.
  */
 val RENDERING_DISPATCHERS = ConcurrentHashMap<Application, JMonkeyRenderingDispatcher>()
 
 /**
- * A cache of physics dispatchers, keyed by [PhysicsSpace]. To ensure the next call to [physicsDispatcher] returns
- * a new dispatcher, remove the map entry for the physics space from here.
+ * A cache of physics dispatchers, keyed by [PhysicsSpace]. To ensure the next call to
+ * [physicsDispatcher] returns a new dispatcher, remove the map entry for the physics space from
+ * here.
  */
 private val PHYSICS_DISPATCHERS = ConcurrentHashMap<PhysicsSpace, JMonkeyPhysicsDispatcher>()
 
 /**
- * Gets a [CoroutineDispatcher] that executes coroutines on the rendering thread of this [Application].
+ * Gets a [CoroutineDispatcher] that executes coroutines on the rendering thread of this
+ * [Application].
  *
  * If a dispatcher already exists for this application then the existing instance is returned,
  * otherwise a new one is created. Calls to this function are always thread safe.
@@ -43,7 +45,8 @@ fun Application.renderingDispatcher(): CoroutineDispatcher =
     RENDERING_DISPATCHERS.getOrPut(this) { JMonkeyRenderingDispatcher(this) }
 
 /**
- * Gets a [CoroutineDispatcher] that executes coroutines on the physics thread of this [PhysicsSpace].
+ * Gets a [CoroutineDispatcher] that executes coroutines on the physics thread of this
+ * [PhysicsSpace].
  *
  * If a dispatcher already exists for this physics space then the existing instance is returned,
  * otherwise a new one is created. Calls to this function are always thread safe.
