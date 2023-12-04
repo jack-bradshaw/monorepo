0692f4a9b316a06a669a8377b96e3995def2c3ab
Jack Bradshaw <jack@jack-bradshaw.com>
2023-12-03 21:34:21 -0800

    Updated presubmit frameworking.

--- presubmit/presubmit.sh
+++ presubmit/presubmit.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
-bash $REPO_ROOT/presubmit/manifest.sh
+# Set presubmit to fail if even one check fails.
+set -e
+
+repo_root=$(git rev-parse --show-toplevel)
+
+presubmits=(format_build.sh format_kotlin.sh build.sh test.sh)
+for presubmit in "${presubmits[@]}";
+do
+  bash $repo_root/$presubmit
+done
+
+echo "Presubmit passed!"
