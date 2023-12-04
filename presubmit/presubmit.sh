8b4970a6a1a334411f99a183ab44f96d275e3ad7
Jack Bradshaw <jack@jack-bradshaw.com>
2023-12-03 21:38:40 -0800

    Fixed broken presubmit.

--- presubmit/presubmit.sh
+++ presubmit/presubmit.sh
@@ -8,7 +8,7 @@ repo_root=$(git rev-parse --show-toplevel)
 presubmits=(format_build.sh format_kotlin.sh build.sh test.sh)
 for presubmit in "${presubmits[@]}";
 do
-  bash $repo_root/$presubmit
+  bash $repo_root/presubmit/$presubmit
 done
 
 echo "Presubmit passed!"
