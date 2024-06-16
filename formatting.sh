8de4176ef822eb5a5d376e217d61fe871f347d4d
Jack Bradshaw <jack@jack-bradshaw.com>
2024-06-16 13:55:59 -0700

    Centralized formatting scripts in formatting.sh.

---
+++ formatting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+KTFMT_BIN=$REPO_ROOT/third_party/facebook/ktfmt/ktfmt-0.46-jar-with-dependencies.jar
+
+# Runs ktfmt. The first argument is the directory to target (recursively). If no arg is provided,
+# the entire repository is targeted.
+ktfmt() {
+  java -jar $KTFMT_BIN "${1:-$REPO_ROOT}"
+}
+
+# Runs buildifier across the entire repository.
+buildifier() {
+  bazel run //:buildifier
+}
