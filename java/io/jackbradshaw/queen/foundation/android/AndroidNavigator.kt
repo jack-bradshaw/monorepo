0dac4d6bd79307ab0fb38f1cba77e15482f82148
Jack Bradshaw <jack@jack-bradshaw.com>
2025-02-15 21:40:35 -0800

    Refactor

--- java/io/jackbradshaw/queen/foundation/android/AndroidNavigator.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.queen.foundation.android
-
-import android.content.Intent
-import io.jackbradshaw.sustainment.primitives.Sustainable.Operation
-import io.jackbradshaw.queen.ui.platforms.android.AndroidUi
-import io.jackbradshaw.queen.foundation.Navigator
-
-/** A [Navigator] which moves between [AndroidDestination]s. */
-interface AndroidNavigator<A, O : Operation<*>> : Navigator<A, Intent, AndroidUi, AndroidDestination<*>, O>
--- java/io/jackbradshaw/queen/foundation/android/AndroidWingsuit.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.queen.foundation.platforms.android
-
-import android.content.Intent
-import io.jackbradshaw.sustainment.primitives.Sustainable.Operation
-import io.jackbradshaw.queen.ui.platforms.android.AndroidUi
-import io.jackbradshaw.queen.foundation.Wingsuit
-
-/** A (Wingsuit configured for Android. */
-interface AndroidWingsuit : Wingsuit<Intent, AndroidUi, Any, Operation<out Any>>
