c7d338b73302050d48c539d141362fce885c9c4b
Jack Bradshaw <jack@jack-bradshaw.com>
2025-02-15 21:40:35 -0800

    Refactor

--- java/io/jackbradshaw/queen/foundation/Destination.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.queen.foundation
-
-import io.jackbradshaw.queen.sustainment.primitives.Sustainable
-import io.jackbradshaw.sustainment.primitives.Sustainable.Operation
-import io.jackbradshaw.queen.ui.primitives.Usable
-import io.jackbradshaw.queen.ui.primitives.Usable.Ui
-
-/** A location in the application that can be given focus. */
-interface Destination<U : Ui, O: Operation<*>> : Usable<U>, Sustainable<O>
