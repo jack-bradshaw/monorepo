8ba540ed457381b1bd5630eb430db172dbaa1290
Jack Bradshaw <jack@jack-bradshaw.com>
2025-03-07 16:37:58 -0800

    Deleted codestone

--- java/io/jackbradshaw/codestone/ui/platforms/android/AndroidComposeUi.kt
+++
@@ -1,9 +0,0 @@
-package io.jackbradshaw.codestone.ui.platforms.android
-
-import androidx.compose.runtime.Composable
-
-/** An Android UI built with [Jetpack Compose](http://developer.android.com/jetpack/compose). */
-interface AndroidComposeUi : AndroidUi {
-  /** Creates the composition. */
-  @Composable fun composable()
-}
