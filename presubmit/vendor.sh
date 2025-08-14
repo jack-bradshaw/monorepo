935c3765854564b7bff9bf91de83adba1ffc4958
Jack Bradshaw <jackjbradshaw@icloud.com>
2025-08-14 14:52:04 -0700

    Added vendor check presubmit

---
+++ presubmit/vendor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Presubmit check to ensure the dependencies needed to build all targets are vendored.
+
+bazel vendor //...
+
+changed_files=$(git status -s)
+
+if [[ -z $changed_files ]]
+then
+  echo "Presubmit check passed: vendor."
+  return 0
+else
+  echo "Presubmit check failed: vendor."
+  echo "Ensure all necessary dependencies are vendored and committed with \`bazel vendor //...\`"
+  for i in "$changed_files[@]"
+  do
+    echo ${changed_files[i]}
+  done
+  return 1
+fi
