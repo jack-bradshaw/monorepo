938e3fa5fc76553e9c6ec28ec0e811347a5cd5b4
Jack Bradshaw <jackjbradshaw@icloud.com>
2025-08-21 12:15:49 -0700

    Migrated otter library to merged tree structure
    This aligns otter with https://docs.google.com/document/d/137li80MXUqCHc6vwfPUUhWY2-eQgdzpwd-DyGBEaC9g/edit?tab=t.0
    
    - Migrated library.
    - Deleted dead code.
    - Deleted javatests directory.

--- presubmit/format_kotlin.sh
+++ presubmit/format_kotlin.sh
@@ -6,7 +6,6 @@ echo "Starting check: format_kotlin"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 source $REPO_ROOT/formatting/formatting.sh
 ktfmt java
-ktfmt javatests
 
 # Ignore changes to 3P to prevent new deps and 3P code from failing presubmit.
 changed_files=$(git status -s | grep -v "third_party")
