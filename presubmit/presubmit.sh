fa118fe38f364970eb76f8cc3bcd3461d90fcac9
Jack Bradshaw <jackjbradshaw@icloud.com>
2025-08-29 15:00:03 -0700

    Migrated to aspect_rules_lint for formatting
    - Installed aspect_rules_lint formatter
    - Added exclusions via .gitignore
    - Deleted existing buildifier integration
    - Deleted existing ktfmt integration
    - Updated formatting presubmit
    - Formatted everything
    - Deleted example npm dependency
    - Replaced some string-based comparisons in tests with object-based comparisons
    
    closes #155

--- presubmit/presubmit.sh
+++ presubmit/presubmit.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 run_presubmit() {
-  echo "Starting presubmit"
+	echo "Starting presubmit"
 
-  # Let presubmit sub-scripts control error handling without immediately failing.
-  set +e
+	# Let presubmit sub-scripts control error handling without immediately failing.
+	set +e
 
-  local repo_root=$(git rev-parse --show-toplevel)
-  local checks=("format_build.sh" "format_kotlin.sh" "build.sh" "test.sh")
+	local repo_root=$(git rev-parse --show-toplevel)
+	local checks=("formatting.sh" "build.sh" "test.sh")
 
-  for check in "${checks[@]}"; do
-    source "$repo_root/presubmit/$check"
-    local check_result=$?
-    if [[ $check_result -ne 0 ]]; then
-      return 1
-    fi
-  done
+	for check in "${checks[@]}"; do
+		source "$repo_root/presubmit/$check"
+		local check_result=$?
+		if [[ $check_result -ne 0 ]]; then
+			return 1
+		fi
+	done
 
-  echo "All presubmit checks passed! 🐥"
+	echo "All presubmit checks passed! 🐥"
 }
