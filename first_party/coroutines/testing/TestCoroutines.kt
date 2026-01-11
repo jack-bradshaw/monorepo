a5a4529c19593c25b5fce38f0af1d25b4178297b
Jack Bradshaw <jack@jack-bradshaw.com>
2026-01-10 20:44:06 -0800

    Feature: subjectest keysnapshot

--- first_party/coroutines/testing/TestCoroutines.kt
+++ first_party/coroutines/testing/TestCoroutines.kt
@@ -3,16 +3,29 @@ package com.jackbradshaw.coroutines.testing
 import com.jackbradshaw.coroutines.Coroutines
 import com.jackbradshaw.coroutines.CoroutinesScope
 import com.jackbradshaw.coroutines.io.testing.TestIoModule
+import com.jackbradshaw.coroutines.testing.advancer.Advancer
+import com.jackbradshaw.coroutines.testing.advancer.AdvancerModule
 import com.jackbradshaw.coroutines.testing.launcher.Launcher
 import com.jackbradshaw.coroutines.testing.launcher.LauncherModule
 import com.jackbradshaw.coroutines.testing.scope.ScopeModule
 import dagger.Component
 import kotlinx.coroutines.test.TestScope
 
+/**
+ * [Coroutines] component for use in tests.
+ * 
+ * The [testScope], [launcher], and [advancer] are integrated such that [Launcher] launches work in
+ * [testScope] and [Advancer] advances the virtual clock of [testScope].
+ * 
+ * 
+ */
 @CoroutinesScope
-@Component(modules = [TestIoModule::class, ScopeModule::class, LauncherModule::class])
+@Component(
+    modules = [TestIoModule::class, ScopeModule::class, LauncherModule::class, AdvancerModule::class])
 interface TestCoroutines : Coroutines {
   fun testScope(): TestScope
 
   fun launcher(): Launcher
+
+  fun advancer(): Advancer
 }
