79080b2c7f41f7b8133731e73173c03461af804e
Jack Bradshaw <jack@jack-bradshaw.com>
2026-01-13 11:22:43 -0800

    Presubmit improvements
    - Fixed bug causing silent failures in formatting presubmit.
    - Refactored presubmit to avoid duplication across scripts.
    - Added presubmit to check markdown files comply with directives.
    - Added "automation" type to directives and updated documentation.
    - Improved disk cleanup before presubmit

---
+++ first_party/presubmit/markdown_directives.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Automations from first_party/contributing/documentation/markdown.md
+
+# Automation: Ordered List Numbering
+if grep -rn --include="*.md" "^[2-9]\.\s\|^[0-9]\{2,\}\.\s" first_party; then
+	echo "Found sequential numbered lists. All ordered list items must use '1.' notation."
+	echo "See: first_party/contributing/documentation/markdown.md (Automation: Ordered List \
+	Numbering)"
+	return 1
+fi
--- first_party/presubmit/test.sh
+++ first_party/presubmit/test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Presubmit check to ensure all tests pass.
 
-echo "Starting check: test"
-
 if ! bazel test //...; then
-	echo "Presubmit check failed: test."
 	echo "Some tests did not pass."
 	return 1
-else
-	echo "Presubmit check passed: test."
-	return 0
 fi
